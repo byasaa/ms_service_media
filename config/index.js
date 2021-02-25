bc6ffc341f64f50130bff4d0282c70ed5e6b6eb6
Muhamad Abiyasa <abi@roots.co.id>
2021-02-25 23:28:37 +0700

    [abi] skeleton

---
+++ config/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const {
+  DB_CONNECTION,
+  DB_HOST,
+  DB_DATABASE,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_PORT,
+} = process.env
+
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_CONNECTION,
+  },
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_CONNECTION,
+  },
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_CONNECTION,
+  },
+}
